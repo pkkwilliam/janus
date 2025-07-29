e1cf51c144b0a6b043202fb9787e028355eb253c
pkkwilliam <24216676+pkkwilliam@users.noreply.github.com>
2025-07-29 11:08:04 -0700

    update the eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,10 +16,12 @@ const eslintConfig = [
       // Turn problematic rules into warnings to allow build to pass
       "react/no-unescaped-entities": "warn",
       "@typescript-eslint/no-unused-vars": "warn",
-      "@typescript-eslint/no-explicit-any": "warn",
+
       "react-hooks/exhaustive-deps": "warn",
       "@typescript-eslint/no-empty-object-type": "warn",
-      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-inferrable-types": "off",
     },
   },
 ];
